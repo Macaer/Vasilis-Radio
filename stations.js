4595867c90e1f0517dbee7d4a6c28bcdbe63715e
Macaer <vasilisoikonomou2@gmail.com>
2025-11-20 18:17:19 +0200

    Add files via upload

--- stations.js
+++ stations.js
@@ -1,50 +1,34 @@
 const stations = [
   {
-    name: "Must Radio",
-    stream: "https://stream.radiomustathens.com/",
-    logo: "logos/must_radio.png",
+    name: "Venus Radio",
+    stream: "http://s7.onweb.gr:8410/;stream.nsv&type=mp3",
+    logo: "http://www.venusradio.gr/images/logo.png",
     description: "House Music",
-    metadataUrl: "https://stream.radiomustathens.com/status-json.xsl",
+    metadataUrl: "",
     category: "House Music"
   },
   {
     name: "Off Radio",
     stream: "https://s3.yesstreaming.net:17062/stream",
-    logo: "logos/off_radio.gif",
+    logo: "https://www.radiofono.gr/sites/www.radiofono.gr/files/images/offradio.gif",
     description: "House Music",
     metadataUrl: "https://s3.yesstreaming.net:17062/status-json.xsl",
     category: "House Music"
   },
   {
-    name: "Up Radio",
-    stream: "https://n04.radiojar.com/9ndpdg3c0s8uv?rj-ttl=5&rj-tok=AAABePParkkAqPgEpo_caHhURw",
-    logo: "logos/up_radio.jpg",
+    name: "Must Radio",
+    stream: "https://stream.radiomustathens.com/",
+    logo: "http://cdn.e-radio.gr/logos/gr/big/radiomust.png",
     description: "House Music",
-    metadataUrl: "https://n04.radiojar.com/9ndpdg3c0s8uv?rj-ttl=5&rj-tok=AAABePParkkAqPgEpo_caHhURw/status-json.xsl",
+    metadataUrl: "https://stream.radiomustathens.com/status-json.xsl",
     category: "House Music"
   },
   {
-    name: "ERT Proto",
-    stream: "https://radiostreaming.ert.gr/ert-proto",
-    logo: "logos/ert_proto.png",
-    description: "Ειδήσεις και πολιτισμός",
-    metadataUrl: "https://radiostreaming.ert.gr/status-json.xsl",
-    category: "ERT Echo"
-  },
-  {
-    name: "Athens FM",
-    stream: "https://example.com/athensfm",
-    logo: "logos/athens_fm.png",
-    description: "Τοπικός σταθμός",
+    name: "Up Radio",
+    stream: "https://n04.radiojar.com/9ndpdg3c0s8uv?rj-ttl=5&rj-tok=AAABePParkkAqPgEpo_caHhURw",
+    logo: "https://www.radiofono.gr/sites/www.radiofono.gr/files/up%20radio%2088.6.jpg",
+    description: "House Music",
     metadataUrl: "",
-    category: "FM Stations"
-  },
-  {
-    name: "Web Chill",
-    stream: "https://example.com/webchill",
-    logo: "logos/web_chill.png",
-    description: "Internet Chillout",
-    metadataUrl: "",
-    category: "Internet Radio"
+    category: "House Music"
   }
 ];
