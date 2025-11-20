e0ae1cbaa654df4b4ff90160bfa16422eff6b0a3
Macaer <vasilisoikonomou2@gmail.com>
2025-11-20 18:32:12 +0200

    Add files via upload

--- stations.js
+++ stations.js
@@ -2,7 +2,7 @@ const stations = [
   {
     name: "Venus Radio",
     stream: "http://s7.onweb.gr:8410/;stream.nsv&type=mp3",
-    logo: "http://www.venusradio.gr/images/logo.png",
+    logo: "logos/venus_radio.png",
     description: "House Music",
     metadataUrl: "",
     category: "House Music"
@@ -10,7 +10,7 @@ const stations = [
   {
     name: "Off Radio",
     stream: "https://s3.yesstreaming.net:17062/stream",
-    logo: "https://www.radiofono.gr/sites/www.radiofono.gr/files/images/offradio.gif",
+    logo: "logos/off_radio.gif",
     description: "House Music",
     metadataUrl: "https://s3.yesstreaming.net:17062/status-json.xsl",
     category: "House Music"
@@ -18,7 +18,7 @@ const stations = [
   {
     name: "Must Radio",
     stream: "https://stream.radiomustathens.com/",
-    logo: "http://cdn.e-radio.gr/logos/gr/big/radiomust.png",
+    logo: "logos/must_radio.png",
     description: "House Music",
     metadataUrl: "https://stream.radiomustathens.com/status-json.xsl",
     category: "House Music"
@@ -26,7 +26,7 @@ const stations = [
   {
     name: "Up Radio",
     stream: "https://n04.radiojar.com/9ndpdg3c0s8uv?rj-ttl=5&rj-tok=AAABePParkkAqPgEpo_caHhURw",
-    logo: "https://www.radiofono.gr/sites/www.radiofono.gr/files/up%20radio%2088.6.jpg",
+    logo: "logos/up_radio.jpg",
     description: "House Music",
     metadataUrl: "",
     category: "House Music"
