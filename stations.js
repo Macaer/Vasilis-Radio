a82dcb409c542451169ecff0d69a26bb215b7dff
Macaer <vasilisoikonomou2@gmail.com>
2025-11-20 14:26:01 +0200

    Add files via upload

---
+++ stations.js
@@ -0,0 +1,14 @@
+const stations = [
+  {
+    "name": "Must Radio",
+    "stream": "https://stream.radiomustathens.com/",
+    "logo": "logos/must_radio.png",
+    "description": "House Music"
+  },
+  {
+    "name": "Off Radio",
+    "stream": "https://s3.yesstreaming.net:17062/stream",
+    "logo": "logos/off_radio.gif",
+    "description": "House Music"
+  }
+];
