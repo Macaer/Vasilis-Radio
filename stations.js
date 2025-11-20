7c1e2dc871e1124e75da3da847178cf0fb4e73d5
Macaer <vasilisoikonomou2@gmail.com>
2025-11-20 16:19:49 +0200

    Add files via upload

--- stations.js
+++ stations.js
@@ -4,13 +4,39 @@ const stations = [
     stream: "https://stream.radiomustathens.com/",
     logo: "logos/must_radio.png",
     description: "House Music",
-    metadataUrl: "https://stream.radiomustathens.com/status-json.xsl"
+    metadataUrl: "https://stream.radiomustathens.com/status-json.xsl",
+    category: "House Music"
   },
   {
     name: "Off Radio",
     stream: "https://s3.yesstreaming.net:17062/stream",
     logo: "logos/off_radio.gif",
     description: "House Music",
-    metadataUrl: "https://s3.yesstreaming.net:17062/status-json.xsl"
+    metadataUrl: "https://s3.yesstreaming.net:17062/status-json.xsl",
+    category: "House Music"
+  },
+  {
+    name: "ERT Proto",
+    stream: "https://radiostreaming.ert.gr/ert-proto",
+    logo: "logos/ert_proto.png",
+    description: "Ειδήσεις και πολιτισμός",
+    metadataUrl: "https://radiostreaming.ert.gr/status-json.xsl",
+    category: "ERT Echo"
+  },
+  {
+    name: "Athens FM",
+    stream: "https://example.com/athensfm",
+    logo: "logos/athens_fm.png",
+    description: "Τοπικός σταθμός",
+    metadataUrl: "",
+    category: "FM Stations"
+  },
+  {
+    name: "Web Chill",
+    stream: "https://example.com/webchill",
+    logo: "logos/web_chill.png",
+    description: "Internet Chillout",
+    metadataUrl: "",
+    category: "Internet Radio"
   }
 ];
