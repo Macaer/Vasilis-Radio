cd3a761ae7a6310f50c2e2ea2348d3565c3ef1ef
Macaer <vasilisoikonomou2@gmail.com>
2025-11-20 15:47:01 +0200

    Add files via upload

--- stations.js
+++ stations.js
@@ -1,14 +1,16 @@
 const stations = [
   {
-    "name": "Must Radio",
-    "stream": "https://stream.radiomustathens.com/",
-    "logo": "logos/must_radio.png",
-    "description": "House Music"
+    name: "Must Radio",
+    stream: "https://stream.radiomustathens.com/",
+    logo: "logos/must_radio.png",
+    description: "House Music",
+    metadataUrl: "https://stream.radiomustathens.com/status-json.xsl"
   },
   {
-    "name": "Off Radio",
-    "stream": "https://s3.yesstreaming.net:17062/stream",
-    "logo": "logos/off_radio.gif",
-    "description": "House Music"
+    name: "Off Radio",
+    stream: "https://s3.yesstreaming.net:17062/stream",
+    logo: "logos/off_radio.gif",
+    description: "House Music",
+    metadataUrl: "https://s3.yesstreaming.net:17062/status-json.xsl"
   }
 ];
